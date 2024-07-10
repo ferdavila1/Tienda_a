6b73ff2c4c0f9d749060c7428cb07ca0e4a1d031
Ferna707 <fernandojosedavilaherrera0@gmail.com>
2024-07-09 22:01:13 -0600

    Hola funcionara?

---
+++ Documents/NetBeansProjects/NuevoEats-main/ProyectoEats/src/main/java/com/mycompany/proyectoeats/ProyectoEats.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoeats;
+
+/**
+ *
+ * @author Lizeth
+ */
+public class ProyectoEats {
+
+    public static void main(String[] args) {
+        System.out.println("Hello yoooooooooo!");
+    }
+}
