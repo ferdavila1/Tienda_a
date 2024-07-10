adc756afc2fb7cca5ae76924e6ed446922a5bae8
Ferna707 <fernandojosedavilaherrera0@gmail.com>
2024-07-09 21:23:36 -0600

    Hola

---
+++ Documents/NetBeansProjects/ProyectoEats-master/src/main/java/com/mycompany/eatsproyecto/EatsProyecto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eatsproyecto;
+
+/**
+ *
+ * @author Lizeth
+ */
+public class EatsProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+         System.out.println("Holaaa!");
+         System.out.println("Holaaax2!");
+          System.out.println("Holaaax3!");
+          System.out.println("Holaaax4!");
+    }
+}
